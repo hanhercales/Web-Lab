25d9cc9ae27476c61bc429e9554aa4354ec7d2ef
Han <147114637+hanhercales@users.noreply.github.com>
2025-09-24 07:29:42 +0700

    commit cai lon

--- obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abf5154dc97fc3dd2c7ec5d5c99bc72ee7d3cc34")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00c6a98ce9329ce8eb513fb636e4b4336ed98606")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
